f7c6d5a54638a49852f4d0ed4a560f5e01de52cb
Mirian Ejibia <mirian.ejibia@omniitg.com>
2026-06-04 10:47:39 +0200

    Add Dashboard page

--- client/src/pages/dashboard/DashboardPage.tsx
+++ client/src/pages/dashboard/DashboardPage.tsx
@@ -2,6 +2,14 @@ import { useSelector } from "react-redux";
 import { fetchEvents, EventsListSelector } from "../../features/events/slice";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../store";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export const DashboardPage = () => {
   const dispatch = useAppDispatch();
@@ -11,5 +19,33 @@ export const DashboardPage = () => {
   }, [dispatch]);
 
   const events = useSelector(EventsListSelector);
-  return <>{events.map((ev) => ev.name)}</>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Description</TableHead>
+          <TableHead>City</TableHead>
+          <TableHead>Country</TableHead>
+          <TableHead>Start Date</TableHead>
+          <TableHead>End Date</TableHead>
+          <TableHead>Status</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {events.map((ev) => (
+          <TableRow key={ev.id}>
+            <TableCell>{ev.name}</TableCell>
+            <TableCell>{ev.description}</TableCell>
+            <TableCell>{ev.city}</TableCell>
+            <TableCell>{ev.country}</TableCell>
+            <TableCell>{new Date(ev.startDate).toLocaleDateString()}</TableCell>
+            <TableCell>{new Date(ev.endDate).toLocaleDateString()}</TableCell>
+            <TableCell>{ev.isCancelled ? "Cancelled" : "Active"}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
